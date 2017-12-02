066382266d05b59970af43accbaa78772a7dcb63
Xintong (zncu) <xintongc@gmail.com>
2017-12-01 20:22:39 -0500

    Update Coordinate.rb

--- Coordinate.rb
+++ Coordinate.rb
@@ -32,6 +32,7 @@ class Coordinate
 end
 
 
+
 first_co = Coordinate.new(4,4)
 second_co = Coordinate.new(2,2)
 puts Coordinate.total
@@ -40,3 +41,45 @@ first_co.setx(9)
 first_co.sety(6)
 puts first_co.to_s
 puts first_co.getx
+
+class XYZCoordinate < Coordinate
+  attr_accessor :z
+  @@newtotal = 0
+  def initialize (x,y,z)
+    super(x,y)
+    @z = z
+    @@newtotal +=1
+  end
+  def to_s
+    return "(#{@x},#{@y},#{@z})"
+  end
+
+  def XYZCoordinate.total
+    return "Number of 3D-coordinates: #@@newtotal"
+  end
+
+end
+
+
+p1 = XYZCoordinate.new(1,2,3)
+puts p1.to_s
+p2 = XYZCoordinate.new(2,3,4)
+p2.setx(22)
+puts p2.to_s
+
+#Ruby allows us to extend specific instances with new behavior
+
+def p1.whatIam
+  return "The oringin on the 3D system"
+end
+
+puts p1.whatIam
+
+
+ObjectSpace.each_object(Coordinate) { |p|
+  puts p.inspect
+}
+
+puts p1.instance_variables
+
+puts XYZCoordinate.superclass
